4eaa3357eaf70b2a43caed94a0928f5bcb20bfd4
Jaden-Giordano <thejadenjack@gmail.com>
2015-03-27 19:41:57 -0700

    fixing

---
+++ Source/miniLD58/miniLD58.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "miniLD58.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, miniLD58, "miniLD58" );
---
+++ Source/miniLD58/miniLD58GameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "miniLD58.h"
+#include "miniLD58GameMode.h"
+
+
+
+
